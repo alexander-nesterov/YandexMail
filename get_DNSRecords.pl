e9b83bb0b8b3766f64a251b73a6178978dac4a0b
Alexander Nesterov <0x12ee705@gmail.com>
2016-09-29 10:09:27 +0700

    Initial Commit

---
+++ get_DNSRecords.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use Data::Dumper;
+use Getopt::Long;
+use JSON qw(decode_json);
+
+#./get_DNSRecords.pl --domain your_domain --token your_token
+
+my $domain = "";
+my $pddtoken = "";
+my $page = 1;
+my $on_page = 50;
+
+&Main();
+
+sub Main
+{
+    system("clear");
+    GetOptions ("domain=s" => \$domain, "token=s" => \$pddtoken) or die("Error in command line arguments\n");
+    &PrintAllEmail();
+}
+
+sub SetURL
+{
+    return "https://pddimp.yandex.ru/api2/admin/dns/list?domain=$domain";
+}
+
+sub PrintAllEmail
+{
+    my $ua = LWP::UserAgent->new;
+    my $url = &SetURL();
+
+    my $response = $ua->get($url, "PddToken" => $pddtoken);
+
+    if ($response->is_success)
+    {
+	my $decoded = decode_json($response->decoded_content);
+	print "Domain: " . $decoded->{"domain"} . "\r\n";
+	print Dumper($decoded);
+	print "DNS Records:\r\n";
+	my $result = $decoded->{"records"};
+	my $i = 1;
+
+	foreach my $record(@{$result})
+	{
+	    print "$i)====\r\n";
+	    print "\ttype: "		. $record->{type}	. "\r\n";
+	    print "\trecord_id: "	. $record->{record_id}	. "\r\n";
+	    print "\tdomain: "		. $record->{domain}	. "\r\n";
+	    print "\tfqdn: "		. $record->{fqdn}	. "\r\n";
+	    print "\tttl: "		. $record->{ttl}	. "\r\n";
+	    print "\tsubdomain: "	. $record->{subdomain}	. "\r\n";
+	    print "\tcontent: "		. $record->{content}	. "\r\n";
+	    print "\tpriority: "	. $record->{priority}	. "\r\n";
+	    $i++;
+	}
+    }
+    else
+    {
+	print $response->status_line;
+    }
+}
+
---
+++ get_ListMail.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use Data::Dumper;
+use Getopt::Long;
+use JSON qw(decode_json);
+
+#./get_ListMail.pl --domain your_domain --token your_token
+
+my $domain = "";
+my $pddtoken = "";
+my $page = 1;
+my $on_page = 50;
+
+&Main();
+
+sub Main
+{
+    system("clear");
+    GetOptions ("domain=s" => \$domain, "token=s" => \$pddtoken) or die("Error in command line arguments\n");
+    &PrintAllEmail();
+}
+
+sub SetURL
+{
+    return "https://pddimp.yandex.ru/api2/admin/email/list?domain=$domain&page=$page&on_page=$on_page";
+}
+
+sub PrintAllEmail
+{
+    my $ua = LWP::UserAgent->new;
+    my $url = &SetURL();
+
+    my $response = $ua->get($url, "PddToken" => $pddtoken);
+
+    if ($response->is_success)
+    {
+	my $decoded = decode_json($response->decoded_content);
+	print "Domain: " . $decoded->{"domain"} . "\r\n";
+	print "Logins:\r\n";
+	my $result = $decoded->{"accounts"};
+	my $i = 1;
+	foreach my $login(@{$result})
+	{
+	    print "$i) " . $login->{login} . "\r\n";
+	    $i++;
+	}
+    }
+    else
+    {
+	print $response->status_line;
+    }
+}
+
